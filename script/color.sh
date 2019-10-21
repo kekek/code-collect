3f4391cb88cc116ad8c1046667e260ee9c26bc77
jinyf <jyf0231@163.com>
2019-10-22 00:22:29 +0800

    add color.sh

---
+++ script/color.sh
@@ -0,0 +1,8 @@
+  ############ color default ###############
+     COLOR_NORMAL="\033[0m"
+     COLOR_GREEN="\033[1;32m"
+     COLOR_YELLOW="\033[1;33m"
+     COLOR_RED="\033[1;33m"
+     COLOR_GREY="\033[1;30m"
+
+     ############ color default ###############
