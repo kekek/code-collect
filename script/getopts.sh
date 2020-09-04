7582f967d9c2bc72a94799b71f5ba6d908addcda
jinyf <jyf0231@163.com>
2020-09-04 14:55:55 +0800

    update getopts.sh

--- script/getopts.sh
+++ script/getopts.sh
@@ -2,30 +2,63 @@
 
 ## getopts
 
-## https://sookocheff.com/post/bash/parsing-bash-script-arguments-with-shopts/
 
-usage="$(basename "$0") [-h] [-s n] -- program to calculate the answer to life, the universe and everything
+## doc https://www.jianshu.com/p/7ebe1dbdc316
 
-where:
-    -h  show this help text
-    -s  set the seed value (default: 42)"
+package=""  # Default to empty package
+target=""  # Default to empty target
 
-seed=42
-while getopts ':hs:' option; do
-  case "$option" in
-    h) echo "$usage"
-       exit
-       ;;
-    s) seed=$OPTARG
-       ;;
-    :) printf "missing argument for -%s\n" "$OPTARG" >&2
-       echo "$usage" >&2
-       exit 1
-       ;;
-   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
-       echo "$usage" >&2
-       exit 1
-       ;;
-  esac
+while getopts ":h" opt; do
+    case ${opt} in
+        h )
+            echo "Usage:"
+            echo "      $(basename "$0") <command> [options]"
+            echo ""
+            echo "Commands"
+            echo "      install     Install a Python package."
+            echo ""
+            echo "General Options:"
+            echo "      -h          Show help."
+            echo "      -t          Location where the package to install"
+            exit 0
+        ;;
+        \? )
+            echo "Invalid Option: -$OPTARG" 1>&2
+            exit 1
+        ;;
+    esac
 done
-shift $((OPTIND - 1))
+shift $((OPTIND -1))  # remove options
+
+subcommand=$1; shift  # Remove 'pip' from the argument list
+
+echo "## $@"
+echo "## $subcommand"
+
+case "$subcommand" in
+# Parse options to the install sub command
+    "install")
+        package=$1; shift  # Remove 'install' from the argument list
+
+    echo "## $package"
+    echo "## $@"
+        # Process package options
+        while getopts ":t:" opt; do
+            case ${opt} in
+                "t" )
+                    target=$OPTARG
+                    echo "install $package at $target"
+                ;;
+                \? )
+                    echo "Invalid Option: -$OPTARG" 1>&2
+                    exit 1
+                ;;
+                : )
+                    echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+                    exit 1
+                ;;
+            esac
+        done
+        shift $((OPTIND -1))
+    ;;
+esac
